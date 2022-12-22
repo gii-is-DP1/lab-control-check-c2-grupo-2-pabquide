dec403f9f162043c3b295f61a240d385ca2b1b98
pabquide <48489345+pabquide@users.noreply.github.com>
2022-12-22 12:22:08 +0100

    Commit
    :-1:

--- src/main/java/org/springframework/samples/petclinic/care/CareProvisionRepository.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareProvisionRepository.java
@@ -3,11 +3,15 @@ package org.springframework.samples.petclinic.care;
 import java.util.List;
 import java.util.Optional;
 
-public interface CareProvisionRepository{
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CareProvisionRepository extends CrudRepository{
     List<CareProvision> findAll();        
     Optional<CareProvision> findById(int id);
     CareProvision save(CareProvision p);
-	//List<Care> findAllCares();
+	List<Care> findAllCares();
     //List<Care> findCompatibleCares(PetType petType, Care additionalCare);
     //Care findCareByName(String name);
     //List<CareProvision> findCaresProvidedByVisitId(Integer visitId);
